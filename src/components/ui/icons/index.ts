58eb165550c9703ba881e107ce7bf8648c3e6a8d
Ochaa <mushoddaqt@gmail.com>
2025-05-04 17:22:00 +0700

    feat: add EyeIcon and EyeOffIcon components for password visibility toggle

---
+++ src/components/ui/icons/index.ts
@@ -0,0 +1,2 @@
+export { default as EyeIcon } from './EyeIcon.vue';
+export { default as EyeOffIcon } from './EyeOffIcon.vue';
--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -4,6 +4,7 @@ import { ref } from 'vue';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { useLoginMutation } from '@/composable/mutations/useAuthMutation';
+import { EyeIcon, EyeOffIcon } from '@/components/ui/icons';
 
 // State
 const username = ref('');
@@ -62,44 +63,8 @@ const togglePasswordVisibility = () => {
               @click="togglePasswordVisibility"
               class="absolute inset-y-0 right-0 flex items-center px-3 text-gray-500 hover:text-gray-700 focus:outline-none"
             >
-              <!-- Show password icon when password is hidden -->
-              <svg
-                v-if="!showPassword"
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                />
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                />
-              </svg>
-              <!-- Hide password icon when password is shown -->
-              <svg
-                v-else
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7A9.97 9.97 0 014.02 8.971m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
-                />
-              </svg>
+              <EyeIcon v-if="!showPassword" />
+              <EyeOffIcon v-else />
             </button>
           </div>
         </div>
