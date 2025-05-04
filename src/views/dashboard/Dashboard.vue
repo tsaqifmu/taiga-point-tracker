078490e7fbe7fb489843cd3e8512a963e0cdd48b
Ochaa <mushoddaqt@gmail.com>
2025-05-04 10:04:51 +0700

    feat: implement logout functionality in dashboard component

--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -1,5 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from '@/components/ui/button';
+import { useAuthStore } from '@/stores/auth';
+
+const authStore = useAuthStore();
+
+const handleLogout = () => {
+  authStore.logout();
+};
+</script>
 
 <template>
-  <div>Ini adalah halaman dashboard</div>
+  <div>this is dashboard page</div>
+  <div>
+    <Button @click="handleLogout" class="text-white"> Logout </Button>
+  </div>
 </template>
